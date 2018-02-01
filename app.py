0dc3b468d8156b3d3d629a735026b5841b9f8559
mariel18-meet <mariel-basous@hotmail.com>
2018-02-01 17:57:16 +0200

    deploy issues

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_heroku import Heroku
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+heroku = Heroku(app)
 db=SQLAlchemy(app)
 
 class Users(db.Model):
